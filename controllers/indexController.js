c731a5f2cd151043093d8fecdefa860dc1526c48
sarfroz <72331821+smsarfroz@users.noreply.github.com>
2025-06-27 03:07:03 +0000

    add custom endsWith function

--- controllers/indexController.js
+++ controllers/indexController.js
@@ -2,6 +2,17 @@ import db from "../db/queries.js"
 import asyncHandler from "express-async-handler";
 import { body, query, validationResult } from "express-validator";
 
+Object.defineProperty(String.prototype, 'endsWith', {
+    enumerable: false,
+    configurable: false,
+    writable: false,
+    value: function (searchString, position) {
+        position = position || this.length;
+        position = position - searchString.length;
+        var lastIndex = this.lastIndexOf(searchString);
+        return lastIndex !== -1 && lastIndex === position;
+    }
+});
 const validatenewProgrammer = [
     body('name')
         .trim()
